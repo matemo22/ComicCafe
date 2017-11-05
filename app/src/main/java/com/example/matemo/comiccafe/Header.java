9d98be44e5de20d81f14b5289721b8232bd011d4
matemo22 <matemo2204@gmail.com>
2017-11-05 11:47:54 +0700

    Header

---
+++ app/src/main/java/com/example/matemo/comiccafe/Header.java
@@ -0,0 +1,13 @@
+package com.example.matemo.comiccafe;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Header extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_header);
+    }
+}
