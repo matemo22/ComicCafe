db04ef79958f821d89a9f3ec91774908af923e5f
matemo22 <matemo2204@gmail.com>
2017-11-08 01:40:22 +0700

    Book Pop Up

--- app/src/main/java/com/example/matemo/comiccafe/CustomGrid.java
+++ app/src/main/java/com/example/matemo/comiccafe/CustomGrid.java
@@ -1,12 +1,17 @@
 package com.example.matemo.comiccafe;
 
 import android.content.Context;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.widget.PopupMenu;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 /**
  * Created by Jeffrey Phinardi on 11/6/2017.
@@ -49,10 +54,42 @@ public class CustomGrid extends BaseAdapter {
             grid = inflater.inflate(R.layout.grid_single, null);
             TextView tit = (TextView) grid.findViewById(R.id.grid_Title);
             TextView au = (TextView) grid.findViewById(R.id.grid_Author);
-            ImageView imageView = (ImageView) grid.findViewById(R.id.grid_image);
+            ImageView imageView = (ImageView) grid.findViewById(R.id.grid_Image);
+            ImageView more = grid.findViewById(R.id.grid_More);
+            LinearLayout layout = grid.findViewById(R.id.grid_Layout);
             tit.setText(titleB[i]);
             au.setText(authorB[i]);
             imageView.setImageResource(Imageid[i]);
+
+            final View finalGrid = grid;
+            more.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    PopupMenu popupMenu = new PopupMenu(finalGrid.getContext(), view);
+                    popupMenu.inflate(R.menu.book_popup_menu);
+                    popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                        @Override
+                        public boolean onMenuItemClick(MenuItem item) {
+                            int id = item.getItemId();
+                            TextView a = finalGrid.findViewById(R.id.grid_Title);
+                            if(id==R.id.popUpReadNow)
+                            {
+                                Toast.makeText(finalGrid.getContext(), "Read Now!"+a.getText().toString(), Toast.LENGTH_SHORT).show();
+                            }
+                            else if(id==R.id.popUpFavorite)
+                            {
+                                Toast.makeText(finalGrid.getContext(), "Favorite!"+a.getText().toString(), Toast.LENGTH_SHORT).show();
+                            }
+                            else if(id==R.id.popUpLike)
+                            {
+                                Toast.makeText(finalGrid.getContext(), "Like!"+a.getText().toString(), Toast.LENGTH_SHORT).show();
+                            }
+                            return false;
+                        }
+                    });
+                    popupMenu.show();
+                }
+            });
         }
         else
         {
